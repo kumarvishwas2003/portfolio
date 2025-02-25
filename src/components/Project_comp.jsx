60eca873995b70b98aacc2e1a3db1837c3544d09
vishwas <vishwas452003@gmail.com>
2025-02-25 06:04:30 +0530

    added links of the projects

--- src/components/Project_comp.jsx
+++ src/components/Project_comp.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const Project_comp = ({name,image,description}) => {
-    return (
-      <>
-        <div className="rounded-lg w-full mt-2  border-green-500 border-2 shadow-lg shadow-green-500/50">
-          <div className="p-[1px] flex items-center justify-center">
-            {/* <div className="rounded-full bg-blue-300 w-32 h-32 flex justify-center items-center">
+const Project_comp = ({ name, image, description, live, github }) => {
+  return (
+    <>
+      <div className="rounded-lg w-full mt-2  border-green-500 border-2 shadow-lg shadow-green-500/50">
+        <div className="p-[1px] flex items-center justify-center">
+          {/* <div className="rounded-full bg-blue-300 w-32 h-32 flex justify-center items-center">
               <span className="text-4xl font-bold text-green-500">BB</span>
             </div> */}
-            <img src={image} alt="" className='w-full object-cover bg-white'/>
-          </div>
-          <div className="bg-black p-4 rounded-b-lg">
-            <h2 className="text-2xl text-green-500 font-bold">{name}</h2>
-            <p className="mt-2 text-green-500">
-              {description}
-              <a
-                href="#"
-                className="text-green-300 underline hover:text-green-600"
-              >
-                Load more.
-              </a>
-            </p>
-          </div>
-          <div className="bg-black px-4 py-2 rounded-b-lg border-t-2 border-green-400 flex justify-between items-center">
-            <span className="text-green-500">LIVE</span>
-            <span className="text-green-500">GITHUB</span>
-          </div>
+          <img src={image} alt="" className="w-full object-cover bg-white" />
         </div>
-      </>
-    );
-}
+        <div className="bg-black p-4 rounded-b-lg">
+          <h2 className="text-2xl text-green-500 font-bold">{name}</h2>
+          <p className="mt-2 text-green-500">
+            {description}
+            <a
+              href="#"
+              className="text-green-300 underline hover:text-green-600"
+            >
+              Load more.
+            </a>
+          </p>
+        </div>
+        <div className="bg-black px-4 py-2 rounded-b-lg border-t-2 border-green-400 flex justify-between items-center">
+          <a href={live} target="_blank">
+            <span className="text-green-500">LIVE</span>{" "}
+          </a>
+          <a href={github} target="_blank">
+            <span className="text-green-500">GITHUB</span>{" "}
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Project_comp
+export default Project_comp;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Project_comp from "./Project_comp";
-import courage from '../assets/project_images/courage.jpeg'
-import budget from '../assets/project_images/budget.png'
-import calculator from "../assets/project_images/calculator.jpeg"
+import courage from "../assets/project_images/courage.jpeg";
+import budget from "../assets/project_images/budget.png";
+import calculator from "../assets/project_images/calculator.jpeg";
 import BMI from "../assets/project_images/BMI.jpeg";
 import tic from "../assets/project_images/tic.png";
 const Projects = () => {
@@ -13,30 +13,41 @@ const Projects = () => {
         image: `${courage}`,
         description:
           "A fun and interactive chatbot inspired by the computer from the cartoon 'Courage the Cowardly Dog.' It generates sarcastic, hilarious, and dry humorous responses. The chatbot also features text-to-speech integration to make conversations even more engaging.",
+        live: "https://courage-gpt.vercel.app/",
+        github: "https://github.com/kumarvishwas2003/Courage-GPT",
       },
+
       {
         name: "Budget Buddy",
         image: `${budget}`,
         description:
           "A finance tracker web app that helps users log daily expenses and generate a structured summary, categorized by date and day. The platform also provides the option to download a PDF report for easy financial tracking and analysis.",
+        live: "https://your-live-link.com/budget-buddy", // {/* live ot yet present */}
+        github: "https://github.com/kumarvishwas2003/Budget-Buddy",
       },
       {
         name: "Tic Tac Toe",
         image: `${tic}`,
         description:
           "A digital version of the classic Tic Tac Toe game, allowing two players to compete against each other. The project focuses on user-friendly UI, responsive gameplay.",
+        live: "https://kumarvishwas2003.github.io/TIC-TAC/",
+        github: "https://github.com/kumarvishwas2003/TIC-TAC",
       },
       {
         name: "BMI Calculator",
         image: `${BMI}`,
         description:
           "A simple yet effective Body Mass Index (BMI) calculator built to understand the fundamentals of responsive web design. The tool takes user input (height and weight) and calculates BMI, providing health insights based on predefined BMI categories.",
+        live: "https://kumarvishwas2003.github.io/BMI/",
+        github: "https://github.com/kumarvishwas2003/BMI",
       },
       {
         name: "React Calculator",
         image: `${calculator}`,
         description:
           "A basic yet functional calculator built using React, designed to perform fundamental mathematical operations like addition, subtraction, multiplication, and division. This project served as a hands-on learning experience for React state management and UI design.",
+        live: "https://main--react-calculator-dark-mode.netlify.app/",
+        github: "https://github.com/kumarvishwas2003/react-calculator",
       },
     ],
   };
@@ -58,6 +69,8 @@ const Projects = () => {
               name={element.name}
               description={element.description}
               image={element.image}
+              live={element.live}
+              github={element.github}
             />
           ))}
         </div>
